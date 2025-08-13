513c14f197f64c219a33225491187106aef32f6e
Gareth McNicol <1968845+gmcnicol@users.noreply.github.com>
2025-08-13 22:29:40 +0300

    remove unused app route group (#143)

--- apps/web/src/app/(app)/story/[id]/split/page.tsx
+++
@@ -1,7 +0,0 @@
-import SplitEditor from "@/components/SplitEditor";
-import { getStory } from "@/lib/stories";
-
-export default async function SplitPage({ params }: { params: { id: string } }) {
-  const story = await getStory(Number(params.id));
-  return <SplitEditor story={story} />;
-}
